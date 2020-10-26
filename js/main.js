70d5c34c92db2c9c339742a4a570564437f95202
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-10-25 23:26:36 -0300

    cap 5 done

--- js/main.js
+++ js/main.js
@@ -33,41 +33,16 @@ var title = document.querySelector('.title');
     }
 
     if(pesoEhValido && alturaEhValido) {
-      var imc = Peso / (Altura * Altura)
-      resImc.textContent = imc.toFixed(2);
+      var imc = calculaImc(Peso, Altura)
+      resImc.textContent = imc;
     }
   }
 
+  function calculaImc(Peso, Altura) {
+    var imc = 0;
+
+    imc = Peso / ( Altura * Altura );
+
+    return imc.toFixed(2);
+  }
   
-  var botaoAdicionar = document.querySelector('#adicionar-paciente');
-  botaoAdicionar.addEventListener('click', (evento) => {
-    evento.preventDefault();
-
-    var form = document.querySelector('#form-adiciona');
-
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement('tr');
-
-    var nomeTd = document.createElement('td');
-    var pesoTd = document.createElement('td');
-    var alturaTd = document.createElement('td');
-    var gorduraTd = document.createElement('td');
-    var imcTd = document.createElement('td');
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector('#tabela-pacientes');
-    tabela.appendChild(pacienteTr);
-  })
